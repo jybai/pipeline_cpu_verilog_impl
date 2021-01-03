0209e226c552d4d56ac52b947ff2dd0bfb32885e
Wei-I Lin <empennage98@gmail.com>
2021-01-03 12:27:24 +0800

    chg: [dcache_sram.v] read from sram done

--- CA2020_project2/codes/dcache_sram.v
+++ CA2020_project2/codes/dcache_sram.v
@@ -16,7 +16,7 @@ module dcache_sram
 input              clk_i;
 input              rst_i;
 input    [3:0]     addr_i;
-input    [24:0]    tag_i;
+input    [24:0]    tag_i; 
 input    [255:0]   data_i;
 input              enable_i;
 input              write_i;
@@ -31,6 +31,11 @@ reg      [24:0]    tag [0:15][0:1];
 reg      [255:0]   data[0:15][0:1];
 
 integer            i, j;
+
+// to contoller interface
+// assign    valid_i = tag_i[24];
+// assign    dirty_i = tag_i[23];
+// assign    tag_i   = tag_i[22:0];
 
 
 // Write Data      
@@ -47,10 +52,33 @@ always@(posedge clk_i or posedge rst_i) begin
     end
     if (enable_i && write_i) begin
         // TODO: Handle your write of 2-way associative cache + LRU here
+
+        // Write to addr with data
+        // tag
+        // valid
+        // dirty
     end
 end
 
 // Read Data      
 // TODO: tag_o=? data_o=? hit_o=?
+always @(*) begin
+    if (tag[addr_i][0][22:0] == tag_i[22:0] && tag[addr_i][0][24] == tag_i[24]) begin
+        tag_o = tag[addr_i][0];
+        data_o = data[addr_i][0];
+        hit_o = 1;
+    end
+    else if (tag[addr_i][1][22:0] == tag_i[22:0] && tag[addr_i][1][24] == tag_i[24]) begin
+        tag_o = tag[addr_i][1];
+        data_o = data[addr_i][1];
+        hit_o = 1;
+    end
+    else begin
+        // Warning: if readmiss => tag and data are all zeros
+        tag_o = 25'b0;
+        data_o = 256'b0;
+        hit_o = 0;
+    end
+end
 
 endmodule
